e992c28cb68fecd3c765506e138425eaed665817
🎀 Ishita Bahamnia <187977436+Ishita95-harvad@users.noreply.github.com>
2025-07-26 00:49:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+import pandas as pd
 
+st.title("AI Smart Grid Dashboard")
+data = pd.read_csv("data/sample_energy_data.csv")
+st.line_chart(data[['solar_generation', 'wind_generation', 'load_demand']])
